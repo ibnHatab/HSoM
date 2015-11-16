a392e80c9978b4ee795fabe995d3d3ac50bdb748
ibnHatab <vlad@leonis.mrc.alcatel.ro>
2015-11-16 20:44:39 +0200

    add static dependenties

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # HSoM
 The Haskell School of Music in Elm
 
+# Local dependenties
+
+$ mkdir -p elm-stuff/packages/ibnHatab/WebMidi/
+$ ln -sf `pwd`/../WebMidi elm-stuff/packages/ibnHatab/WebMidi/1.0.0
+$ ln -sf `pwd`/../WebMidi/elm-stuff/build-artifacts/ibnHatab elm-stuff/build-artifacts/ibnHatab
+
+Add this line to elm-suff/exact-dependencies.json file
+> "ibnHatab/WebMidi": "1.0.0"
--- src/Music.elm
+++ src/Music.elm
@@ -5,11 +5,11 @@ The Musiv Data Type
 -}
 
 type alias Pitch = (PitchClass, Octave)
-type alias PitchClass = Cf | C | Cs
-                      | Df | D | Ds
-                      | Ef | E | Es
-                      | Ff | F | Fs
-                      | Gf | G | Gs
-                      | Af | A | As
-                      | Bf | B | Bs
+type PitchClass = Cf | C | Cs
+                | Df | D | Ds
+                | Ef | E | Es
+                | Ff | F | Fs
+                | Gf | G | Gs
+                | Af | A | As
+                | Bf | B | Bs
 type alias Octave = Int
